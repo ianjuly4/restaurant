9d1e43b543675818a0cd0f8d39e69414ecd46f96
Ian July <ianjuly4@gmail.com>
2024-02-07 16:53:49 -0700

    Form, FormPage css style update

---
+++ restaurant/src/components/FormHeader.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function FormHeader(){
+    return(
+        <h1 className="FormHeader">
+            DON'T SEE YOUR FAVORITE RESTAURANT? ADD IT!!</h1>
+    )
+}
+export default FormHeader
--- restaurant/src/components/FormPage.js
+++ restaurant/src/components/FormPage.js
@@ -1,11 +1,13 @@
 import React from "react"
 import NavBar from "./NavBar"
 import Form from "./Form"
+import FormHeader from "./FormHeader"
 
 function  FormPage(){
     return(
-        <div>
+        <div className="FormPage">
             <NavBar/>
+            <FormHeader />
             <Form />
         </div>
     )
