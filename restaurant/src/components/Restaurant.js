364a0fb0599381bf5ec1593dae33c28d3765fffe
Ian July <ianjuly4@gmail.com>
2024-02-07 23:04:45 -0700

    About page update

--- restaurant/src/components/Restaurant.js
+++ restaurant/src/components/Restaurant.js
@@ -1,12 +1,18 @@
 import React from "react"
 
 function Restaurant({name, address, type, price}){
+  
+  const handleDelete = () => {
+
+  }
+
 return (
     <li className="Restaurant">
       <h3>{name}</h3>
       <h5>{address}</h5>
-      <h5>{type}</h5>
-      <h5>{price}</h5>
+      <h5>Food Type: {type}</h5>
+      <h5>Price: {price}</h5>
+      <button>Delete</button>
     </li>
   );
 }  
